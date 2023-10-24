7ec3f776750014562e19cab7395e288a3e2ed4ed
James Carlo Luchavez <jamescarloluchavez@gmail.com>
2023-10-24 14:22:43 +0800

    "Refactor SecretController and adjust dependencies in SimpleSecrets"
    Refactored SecretController by removing unnecessary SecretEvents and replacing UnauthorizedException handling with AuthorizationException handling. Several changes to method calls have been made for improved code readability and efficiency. Adjusted dependencies versioning in composer.json for luchavez/starter-kit compatibility. Added CASCADE deletion for the 'owned' column of the secrets table. Added restore to the routes and middleware array in config. Bound 'secret' route to query secret by uuid in the ServiceProvider.

--- database/migrations/2022_12_20_022453_create_secrets_table.php
+++ database/migrations/2022_12_20_022453_create_secrets_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Luchavez\StarterKit\Enums\OnDeleteAction;
 
 return new class() extends Migration
 {
@@ -16,7 +17,7 @@ return new class() extends Migration
         Schema::create('secrets', static function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
-            $table->owned(); // from luchavez/starter-kit
+            $table->owned(on_delete: OnDeleteAction::CASCADE); // from luchavez/starter-kit
             $table->unsignedTinyInteger('type');
             $table->string('value');
             $table->string('description')->nullable();
